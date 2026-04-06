8043d7fc9568234307c919b3ead8ab38faba6524
nastipa <dinnanitipa@gmail.com>
2026-04-06 07:48:34 +0200

    update

--- app.config.js
+++ app.config.js
@@ -22,7 +22,7 @@ export default {
     /* ✅ WEB (PWA SUPPORT ADDED 🔥) */
     /* ===================================================== */
     web: {
-      bundler: "metro",
+      bundler: "webpack",
       output: "static",
       favicon: "./assets/images/icon.png"
     },
